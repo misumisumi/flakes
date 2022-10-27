7f022861bb863f1126eb5354edcae0c447fd3c34
Kobayashi Sumiharu <dragon511southern@gmai.com>
2022-10-27 23:41:31 +0900

    fix path

--- packages/wavesurfer/default.nix
+++ packages/wavesurfer/default.nix
@@ -1,8 +1,8 @@
 { stdenv, lib, name, pkgSources, writeShellScript, makeDesktopItem, tk, tcl, snack }:
 let
-  launcher = writeShellScript "wavesurfer" ''
+  launcher = path: writeShellScript "wavesurfer" ''
     #! /usr/bin/env bash
-    exec $out/src/app-wavesurfer/wavesurfer.tcl "''$@"
+    exec /run/current-system/sw/lib/wavesurfer/src/app-wavesurfer/wavesurfer.tcl "''$@"
   '';
 
   desktopApp = makeDesktopItem {
@@ -19,21 +19,19 @@ stdenv.mkDerivation {
 
   nativeBuildInputs = [ tk tcl snack ];
 
-  outputs = [ "out" "bin" "lib" "doc" ];
-
-  
   installPhase = ''
-    mkdir -p {$lib,$doc}/wavesurfer
+    mkdir -p $out/bin
+    mkdir -p $out/{lib,doc}/wavesurfer
     mkdir -p $out/share/licenses/wavesurfer
     mkdir -p $out/share/icons/hicolor/48x48/apps
     mkdir -p $out/share/applications/
 
-    cp -r demos msgs src tools $lib/wavesurfer/
+    cp -r demos msgs src tools $out/lib/wavesurfer/
     cp -r icons/icon48.xpm $out/share/icons/hicolor/48x48/apps
-    chmod a+x $lib/wavesurfer/src/app-wavesurfer/wavesurfer.tcl
-    cp doc/* $doc/wavesurfer/
+    chmod a+x $out/lib/wavesurfer/src/app-wavesurfer/wavesurfer.tcl
+    cp doc/* $out/doc/wavesurfer/
 
-    install -Dm755 ${launcher} $bin/wavesurfer
+    install -Dm755 ${launcher} $out/bin/wavesurfer
     install LICENSE.txt $out/share/licenses/wavesurfer/LICENSE.txt
   '';
 
