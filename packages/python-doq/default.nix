2c83f4313fc3f69c6fd0f6e7a9923c07b1377974
Kobayashi Sumiharu <dragon511southern@gmai.com>
2022-10-29 23:02:32 +0900

    fix python-doq

--- packages/python-doq/default.nix
+++ packages/python-doq/default.nix
@@ -1,11 +1,12 @@
-{ lib, buildPythonPackage, name, pkgSources }:
-
-buildPythonPackage rec {
+{ lib, pythonPackages, name, pkgSources }:
+let
+  inherit (pythonPackages) buildPythonPackage;
+in buildPythonPackage rec {
   inherit (pkgSources."${name}") pname version src;
 
-  format = "setuptools";
-
   doCheck = false;
+
+  propagatedBuildInputs = with pythonPackages; [ setuptools ];
 
   meta = with lib; {
     homepage = "https://github.com/heavenshell/py-doq";
