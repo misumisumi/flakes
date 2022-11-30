41ac2cc7094c5462b62e6369571bddf035286226
Sumi-Sumi <dragon511southern@gmail.com>
2022-11-30 17:46:23 +0900

    fix xp-pen driver

--- packages/xp-pen-tablet/default.nix
+++ packages/xp-pen-tablet/default.nix
@@ -19,6 +19,7 @@ let
   pkg_url = (with builtins; fromJSON (readFile ../../_sources/generated.json))."xp-pen-tablet".src.url;
   pkg_sha256 = (with builtins; fromJSON (readFile ../../_sources/generated.json))."xp-pen-tablet".src.sha256;
   mkDerivation = libsForQt5.callPackage ({ mkDerivation }: mkDerivation) {};
+  dataDir = "var/lib/xppend1v2";
 in
 mkDerivation rec {
   inherit (pkgSources."${name}") pname version;
@@ -28,47 +29,63 @@ mkDerivation rec {
     name = "xp-pen-tablet-${version}.x86_64.tar.gz";
   };
 
-  nativeBuildInputs = [ autoPatchelfHook libsForQt5.wrapQtAppsHook ];
+  nativeBuildInputs = [
+    wrapQtAppsHook
+    autoPatchelfHook
+  ];
+
+  dontBuild = true;
+
+  dontWrapQtApps = true; # this is done manually
 
   buildInputs = [
-      libusb1
-      libX11
-      libXtst
-      libXi
-      libXrandr
-      libXinerama
-      glibc
-      libGL
-      stdenv.cc.cc.lib
-      libsForQt5.qtx11extras
-    ];
+    libusb1
+    libX11
+    libXtst
+    libXi
+    libXrandr
+    libXinerama
+    glibc
+    libGL
+    stdenv.cc.cc.lib
+    libsForQt5.qtx11extras
+  ];
+  desktopItems = [ 
+    (makeDesktopItem {
+      inherit name;
+      exec = "xp-pen-driver /mini";
+      icon = "pentablet";
+      comment = "XPPen driver";
+      desktopName = "xppentablet";
+      categories = [ "Application" "Utility" ];
+    })
+  ];
 
   installPhase = ''
-    mkdir -p $out/{etc,lib,share}
-
-    cp -r App/etc/* $out/etc
-    cp -r App/lib/* $out/lib
-    cp -r App/usr/lib/* $out/lib
+    runHook preInstall
+    mkdir -p $out/{opt,bin,share}
+    cp -r App/usr/lib/pentablet/{pentablet,resource.rcc,conf} $out/opt
+    chmod +x $out/opt/pentablet
+    cp -r App/lib $out/lib
     cp -r App/usr/share/* $out/share
-
-    chmod 644 $out/lib/udev/rules.d/10-xp-pen.rules
-
-    #make those executable
-    chmod 755 $out/lib/pentablet/pentablet.sh
-    chmod 755 $out/lib/pentablet/pentablet
 
     #fix license permissions
     chmod 644 $out/lib/pentablet/LGPL
 
+    cp $desktopItems/share/applications $out/share/applications
+
     #config is global so everyone needs write access
     chmod 666 $out/lib/pentablet/conf/xppen/config.xml
 
-    #minimize GUI on autostart
-    sed -re 's/(^Exec=\/.+)/\1 \/mini/gi' -i $out/etc/xdg/autostart/xppentablet.desktop
+    runHook postInstall
+  '';
+
+  postFixup = ''
+    makeWrapper $out/opt/pentablet $out/bin/xp-pen-driver \
+      "''${qtWrapperArgs[@]}"
   '';
 
   meta = with lib; {
-    inherit version;
     description = "XP-Pen (Official) Linux utility (New UI driver)";
     homepage = "https://www.xp-pen.com/download/index.html";
     license = licenses.lgpl3Only;
