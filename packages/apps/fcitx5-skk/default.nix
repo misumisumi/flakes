aeb0afb7cb5132c2b282f0ab3e2331b1edb06531
Sumi-Sumi <dragon511southern@gmail.com>
2023-01-13 18:45:41 +0900

    add fcitx5-skk

---
+++ packages/apps/fcitx5-skk/default.nix
@@ -0,0 +1,37 @@
+{ stdenv, lib, name, pkgSources, cmake, extra-cmake-modules, ninja, pkg-config, fcitx5, qt5, libsForQt5, libskk, skk-dicts }:
+
+stdenv.mkDerivation
+{
+  inherit (pkgSources."${name}") pname version src;
+
+  nativeBuildInputs = [ cmake ninja extra-cmake-modules pkg-config ];
+  buildInputs = [ fcitx5 libsForQt5.fcitx5-qt qt5.qtbase libskk skk-dicts ];
+
+  cmakeFlags = [
+    "-GNinja"
+    "-DCMAKE_INSTALL_PREFIX=/share"
+    "-DCMAKE_INSTALL_LIBDIR=/lib"
+  ];
+
+  buildPhase = ''
+    cmake $cmakeFlags .
+    ninja
+  '';
+
+  installPhase = ''
+    DESTDIR=$out ninja install
+  '';
+
+  meta = with lib; {
+    description = "A SKK style input method engine for fcitx5";
+    longDescription = ''
+      Fcitx5-skk is an input method engine for fcitx5. It is based on libskk and thus
+      provides basic features of SKK Japanese input method such as kana-to-kanji conversion,
+      new word registration, completion, numeric conversion, abbrev mode, kuten input,
+      hankaku-katakana input, and re-conversion.
+    '';
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
+  };
+
+}
