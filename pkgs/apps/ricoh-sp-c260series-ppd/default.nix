c129b57d9f0bd558b371f7a6f7f750fc255905c5
Sumi-Sumi <dragon511southern@gmail.com>
2023-04-18 18:07:42 +0900

    fix: package build

--- pkgs/apps/ricoh-sp-c260series-ppd/default.nix
+++ pkgs/apps/ricoh-sp-c260series-ppd/default.nix
@@ -3,39 +3,32 @@
   name,
   pkgSources,
   stdenv,
-  fetchurl,
   dpkg,
-  autoPatchelfHook,
-  makeWrapper,
-}: let
-  arches = ["x86_64" "i686" "armv7l"];
+}:
+stdenv.mkDerivation {
+  inherit (pkgSources."${name}") src version pname;
 
-  name = "cups-brother-hll5100dn";
-in
-  stdenv.mkDerivation {
-    pname = name;
-    inherit (pkgSources."${name}-lpr") version;
+  nativeBuildInputs = [dpkg];
 
-    nativeBuildInputs = [dpkg makeWrapper autoPatchelfHook];
+  unpackPhase = ''
+    runHook preUnpack
+    mkdir $out
+    dpkg-deb -x $src $out
+    runHook postUnpack
+  '';
 
-    unpackPhase = ''
-      runHook preUnpack
-      dpkg-deb -x ${pkgSources."${name}-lpr".src} $out
-      runHook postUnpack
-    '';
+  installPhase = ''
+    runHook preInstall
+    mv $out/usr/* $out
+    rm -rf $out/usr
+    runHook postInstall
+  '';
 
-    installPhase = ''
-      runHook preInstall
-      mv $out/usr/* $out
-      rm -rf $out/usr
-      runHook postInstall
-    '';
-
-    meta = with lib; {
-      homepage = "http://support.ricoh.com/bbv2/html/dr_ut_d/ipsio/history/w/bb/pub_j/dr_ut_d/4101035/4101035832/V100/5205252/sp-c260series-printer-1.00-amd64/history.htm";
-      description = "richo c260seriees printer driver";
-      sourceProvenance = with sourceTypes; [binaryNativeCode];
-      license = licenses.unfree;
-      platforms = platforms.linux;
-    };
-  }
+  meta = with lib; {
+    homepage = "http://support.ricoh.com/bbv2/html/dr_ut_d/ipsio/history/w/bb/pub_j/dr_ut_d/4101035/4101035832/V100/5205252/sp-c260series-printer-1.00-amd64/history.htm";
+    description = "richo c260seriees printer driver";
+    sourceProvenance = with sourceTypes; [binaryNativeCode];
+    license = licenses.unfree;
+    platforms = platforms.linux;
+  };
+}
