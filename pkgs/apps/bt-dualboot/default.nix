e695b45a121ad66d69eda952750de3a08c70b5b8
Sumi-Sumi <dragon511southern@gmail.com>
2023-04-18 17:55:20 +0900

    fix: move to nativeBuildInputs

--- pkgs/apps/bt-dualboot/default.nix
+++ pkgs/apps/bt-dualboot/default.nix
@@ -1,16 +1,22 @@
-{ lib, pythonPackages, python3, name, pkgSources, chntpw }:
-let
+{
+  lib,
+  pythonPackages,
+  python3,
+  name,
+  pkgSources,
+  chntpw,
+}: let
   inherit (pythonPackages) buildPythonApplication;
-in buildPythonApplication rec {
-  inherit (pkgSources."${name}") pname version src;
-  buildInputs = [ chntpw ];
+in
+  buildPythonApplication rec {
+    inherit (pkgSources."${name}") pname version src;
+    nativeBuildInputs = [chntpw];
 
-  doCheck = false;
+    doCheck = false;
 
-  meta = with lib; {
-    homepage = "https://github.com/x2es/bt-dualboot";
-    description = "User-friendly tool making your bluetooth devices working both in Windows and Linux without re-pairing chore.";
-    license = licenses.mit;
-  };
-}
-
+    meta = with lib; {
+      homepage = "https://github.com/x2es/bt-dualboot";
+      description = "User-friendly tool making your bluetooth devices working both in Windows and Linux without re-pairing chore.";
+      license = licenses.mit;
+    };
+  }
