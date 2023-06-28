aec3e0d98211791a2affd0b8dfdd82c18e4249c0
Sumi-Sumi <dragon511southern@gmail.com>
2023-06-29 04:43:27 +0900

    rm: plemoljp-nf

--- pkgs/apps/plemoljp-fonts/default.nix
+++ pkgs/apps/plemoljp-fonts/default.nix
@@ -1,44 +1,48 @@
-{ stdenvNoCC, lib, name, pkgSources, unzip, fonts ? [ ] }:
-let
-  knownFonts = [ "plemoljp-hs" "plemoljp-nfj" "plemoljp-nf" "plemoljp" ];
+{
+  stdenvNoCC,
+  lib,
+  pkgSources,
+  unzip,
+  fonts ? [],
+}: let
+  knownFonts = ["plemoljp-hs" "plemoljp-nf" "plemoljp"];
   selectedFonts =
-    if (fonts == [ ]) then
-      knownFonts
-    else
-      let unknown = lib.subtractLists knownFonts fonts; in
-      if (unknown != [ ]) then
-        throw "Unknown font(s): ${lib.concatStringsSep " " unknown}"
-      else
-        fonts
-  ;
+    if (fonts == [])
+    then knownFonts
+    else let
+      unknown = lib.subtractLists knownFonts fonts;
+    in
+      if (unknown != [])
+      then throw "Unknown font(s): ${lib.concatStringsSep " " unknown}"
+      else fonts;
   srcs = map (fName: pkgSources."${fName}".src) selectedFonts;
 in
-stdenvNoCC.mkDerivation rec {
-  inherit (pkgSources."plemoljp") version;
-  inherit srcs;
-  pname = "plemoljp-fonts";
+  stdenvNoCC.mkDerivation rec {
+    inherit (pkgSources."plemoljp") version;
+    inherit srcs;
+    pname = "plemoljp-fonts";
 
-  nativeBuildInputs = [
-    unzip
-  ];
-  sourceRoot = ".";
-  unpackCmd = "unzip -o $curSrc";
-  buildPhase = ''
-    echo "selected fonts are ${toString selectedFonts}"
-    ls *.otf *.ttf
-  '';
-  installPhase = ''
-    runHook preInstall
+    nativeBuildInputs = [
+      unzip
+    ];
+    sourceRoot = ".";
+    unpackCmd = "unzip -o $curSrc";
+    buildPhase = ''
+      echo "selected fonts are ${toString selectedFonts}"
+      ls *.otf *.ttf
+    '';
+    installPhase = ''
+      runHook preInstall
 
-    find -name \*.ttf -exec mkdir -p $out/share/fonts/truetype/PlemolJP \; -exec mv {} $out/share/fonts/truetype/PlemolJP \;
+      find -name \*.ttf -exec mkdir -p $out/share/fonts/truetype/PlemolJP \; -exec mv {} $out/share/fonts/truetype/PlemolJP \;
 
-    runHook postInstall
-  '';
+      runHook postInstall
+    '';
 
-  meta = with lib; {
-    inherit version;
-    description = "A composite font family of IBM Plex Mono and IBM Plex Sansjp (optional nerd-fonts)";
-    homepage = "https://github.com/yuru7/PlemolJP";
-    license = licenses.ofl;
-  };
-}
+    meta = with lib; {
+      inherit version;
+      description = "A composite font family of IBM Plex Mono and IBM Plex Sansjp (optional nerd-fonts)";
+      homepage = "https://github.com/yuru7/PlemolJP";
+      license = licenses.ofl;
+    };
+  }
