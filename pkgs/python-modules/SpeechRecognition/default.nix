4fdb650ff5b9f05e3b989bb20e4fef77ea622d92
Sumi-Sumi <dragon511southern@gmail.com>
2023-06-29 05:13:45 +0900

    hotfix: add patchelf

--- pkgs/python-modules/SpeechRecognition/default.nix
+++ pkgs/python-modules/SpeechRecognition/default.nix
@@ -3,17 +3,23 @@
   pythonPackages,
   name,
   pkgSources,
-  flac,
   openai,
+  patchelf,
 }: let
   inherit (pythonPackages) buildPythonPackage;
 in
-  buildPythonPackage rec {
+  buildPythonPackage {
     inherit (pkgSources."${name}") pname version src;
+
+    nativeBuildInputs = [patchelf];
 
     doCheck = false;
     # for runtime depend
-    propagatedBuildInputs = [flac openai];
+    propagatedBuildInputs = [openai];
+    postInstall = ''
+      patchelf --set-interpreter ''$(cat ''$NIX_CC/nix-support/dynamic-linker) $out/lib/python*/site-packages/speech_recognition/flac-linux-x86_64
+      patchelf --set-interpreter ''$(cat ''$NIX_CC/nix-support/dynamic-linker) $out/lib/python*/site-packages/speech_recognition/flac-linux-x86
+    '';
 
     meta = with lib; {
       homepage = "https://github.com/Uberi/speech_recognition";
