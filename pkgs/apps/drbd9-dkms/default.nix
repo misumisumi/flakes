9745597c7a872bc48e25c8b95d9d1581a1297254
misumisumi <dragon511southern@gmail.com>
2023-09-29 23:59:25 +0900

    fix(drbd9-dkms): fix kernelVersion

--- pkgs/apps/drbd9-dkms/default.nix
+++ pkgs/apps/drbd9-dkms/default.nix
@@ -13,17 +13,21 @@ stdenv.mkDerivation {
   hardeningDisable = [ "pic" "format" ];
   nativeBuildInputs = [ nukeReferences ] ++ kernel.moduleBuildDependencies;
 
+  kernel = kernel.dev;
+  kernelVersion = kernel.modDirVersion;
+
   makeFlags = [
     "KVER=${kernel.modDirVersion}"
     "KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
     "ARCH=x86"
+    "DESTDIR=$(out)"
   ];
 
   installPhase = ''
-    mkdir -p $out/lib/modules/$kernelVersion/misc
+    mkdir -p $out/lib/modules/$kernelVersion/updates
       for x in $(find . -name '*.ko'); do
         nuke-refs $x
-        cp $x $out/lib/modules/$kernelVersion/misc/
+        cp $x $out/lib/modules/$kernelVersion/updates/
       done
   '';
 
