d9970a74f90c89771d4f0c7d0df328e879d7f051
misumisumi <dragon511southern@gmail.com>
2023-09-29 22:22:01 +0900

    add(drbd9-dkms): Add dkms9 kernel module

---
+++ pkgs/apps/drbd9-dkms/default.nix
@@ -0,0 +1,35 @@
+{ pkgSources
+, name
+, stdenv
+, lib
+, linuxPackages
+, kernel ? linuxPackages.kernel
+}:
+
+stdenv.mkDerivation {
+  inherit (pkgSources."${name}") pname version src;
+
+  hardeningDisable = [ "pic" "format" ];
+  nativeBuildInputs = kernel.moduleBuildDependencies;
+
+  makeFlags = [
+    "KVER=${kernel.modDirVersion}"
+    "KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+    "ARCH=x86"
+  ];
+
+  installPhase = ''
+    mkdir -p $out/lib/modules/$kernelVersion/misc
+      for x in $(find . -name '*.ko'); do
+        nuke-refs $x
+        cp $x $out/lib/modules/$kernelVersion/misc/
+      done
+  '';
+
+  meta = with lib; {
+    description = "A kernel module of drbd9";
+    homepage = "https://github.com/LINBIT/drbd";
+    license = licenses.gpl2;
+    platforms = platforms.linux;
+  };
+}
