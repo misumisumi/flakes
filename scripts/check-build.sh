d275e18e2252305d87a79a443498fb8657a8473f
misumisumi <dragon511southern@gmail.com>
2024-07-19 16:06:36 +0900

    refactor: remove unused script

--- scripts/check-build.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-start=$1
-end=$2
-
-nix flake show --json | jq -r '.packages."x86_64-linux" | keys | .[]' | tail -n+"${start}" | head -n"${end}" | while read -r pkg; do
-  nix build --no-link ".#${pkg}" || exit 1
-done
