4bfde585426a0fc5794aab6e36992937e6317914
misumisumi <sumiharu@misumi-sumi.com>
2025-01-17 02:34:56 +0900

    fix(bt-dualboot): fixed upstream so remove own patch

--- pkgs/apps/bt-dualboot/default.nix
+++ pkgs/apps/bt-dualboot/default.nix
@@ -4,24 +4,13 @@
   name,
   pkgSources,
   chntpw,
-  fetchpatch,
 }:
 let
   inherit (python3Packages) buildPythonApplication;
-  _chntpw = chntpw.overrideAttrs (oldAttrs: {
-
-    patches = oldAttrs.patches ++ [
-      (fetchpatch {
-        name = "17_hexdump-pointer-type.patch";
-        url = "https://git.launchpad.net/ubuntu/+source/chntpw/plain/debian/patches/17_hexdump-pointer-type.patch?id=aed501c87499f403293e7b9f505277567c2f3b52";
-        sha256 = "sha256-ir9LFl8FJq141OwF5SbyVMtjQ1kTMH1NXlHl0XZq7m8=";
-      })
-    ];
-  });
 in
 buildPythonApplication {
   inherit (pkgSources."${name}") pname version src;
-  dependencies = [ _chntpw ];
+  dependencies = [ chntpw ];
 
   doCheck = false;
 
