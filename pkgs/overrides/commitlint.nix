40cc5cb00a043dc637fb4ecca80b275744d8d994
misumisumi <s.kobayashi@misumi-sumi.com>
2025-11-26 20:22:17 +0900

    fix(commitlint): fix not be able to loading node_modules by NODE_PATH

--- pkgs/overrides/commitlint.nix
+++ pkgs/overrides/commitlint.nix
@@ -1,6 +1,8 @@
 { final, prev }:
-with prev; commitlint.overrideAttrs {
-  passthru.withPlugins = p:
+with prev;
+commitlint.overrideAttrs {
+  passthru.withPlugins =
+    p:
     let
       _plugins = p final.nodePackages;
       plugins = map (p: if lib.isDerivation p then p else final.nodePackages.${p}) _plugins;
@@ -14,8 +16,9 @@ with prev; commitlint.overrideAttrs {
         "/lib/node_modules"
       ];
       postBuild = ''
+        #HACK: NODE_PATH is not working since node v22 so I use PREFIX from https://github.com/nodejs/node/blob/4451309e99e37da7d4b44b5fb136db1c6a1dea90/doc/api/modules.md
         wrapProgram "$out/bin/commitlint" \
-          --set NODE_PATH "$out/lib/node_modules"
+          --set PREFIX "$out"
       '';
     };
 }
