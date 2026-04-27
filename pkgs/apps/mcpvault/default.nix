0a6c7e21030f20d7cd1ef66773b4086f6895e2fc
misumisumi <s.kobayashi@misumi-sumi.com>
2026-04-27 17:50:21 +0900

    mcpvault: 1 -> 0-unstable-2026-04-16

--- pkgs/apps/mcpvault/default.nix
+++ pkgs/apps/mcpvault/default.nix
@@ -9,15 +9,15 @@ let
 in
 buildNpmPackage {
   inherit pname;
-  version = "";
+  version = "0-unstable-2026-04-16";
   src = fetchFromGitHub {
     owner = "bitbonsai";
     repo = pname;
-    rev = "32776a5f7fd62526863618b10a85e086deb22db8";
-    sha256 = "sha256-3VCFumEz61m3kat4ACkmlKaoACyB6g5NXlwZBaPG+Rc=";
+    rev = "dec984fee1f5daeac5d6a23ae1d9a62d6318fcb1";
+    sha256 = "sha256-Hd3ml5FmD9/EvhcWfX3AgF1vJQmBQRhRbIb+rW56N8A=";
   };
 
-  npmDepsHash = "sha256-Wcp053R388YGd7sdWtIgJ/DdMkepx+4aymev8jDA/VE=";
+  npmDepsHash = "sha256-D9DCHEw9L2/crhOcp80YB3CRxL6EZ5CxoiiuoN/Xsx0=";
 
   passthru.updateScript = nix-update-script {
     extraArgs = [
