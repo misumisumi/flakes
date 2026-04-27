81dd4855baa4ef0c6f271467d4d5c47352eed798
misumisumi <s.kobayashi@misumi-sumi.com>
2026-04-27 18:32:33 +0900

    proton-ge-rtsp-bin: GE-Proton10-26-rtsp20 -> GE-Proton10-33-rtsp24-1

--- pkgs/apps/proton-ge-rtsp-bin/default.nix
+++ pkgs/apps/proton-ge-rtsp-bin/default.nix
@@ -7,17 +7,19 @@ proton-ge-bin.overrideAttrs (
   let
     pname = "proton-ge-rtsp-bin";
     version = "GE-Proton10-33-rtsp24-1";
+  in
+  {
+    inherit pname version;
     src = fetchTarball {
       url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/download/${version}/${version}.tar.gz";
       sha256 = "sha256-eAJjw575cJlj7qLsPC1LgRsVMW4O754Q6SO7IV74EyE=";
     };
-  in
-  {
-    inherit pname version src;
     passthru = (oldAttrs.passthru or { }) // {
       updateScript = nix-update-script {
         extraArgs = [
           "--flake"
+          "--version-regex"
+          "(GE-Proton.*)"
           "--override-filename"
           "pkgs/apps/proton-ge-rtsp-bin/default.nix"
           "--url"
