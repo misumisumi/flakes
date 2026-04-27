4b49128cc1b542e69c486eaa99d8829fa59fc4f1
misumisumi <s.kobayashi@misumi-sumi.com>
2026-04-27 18:06:49 +0900

    prettier-plugin-toml: 2.0.5 -> 2.0.6

--- pkgs/apps/prettier-plugin-toml/default.nix
+++ pkgs/apps/prettier-plugin-toml/default.nix
@@ -3,12 +3,11 @@
   fetchurl,
   nix-update-script,
   buildNpmPackage,
-  importNpmLock,
 }:
 let
   inherit (lib) licenses;
   pname = "prettier-plugin-toml";
-  version = "1.0.6";
+  version = "2.0.6";
 in
 buildNpmPackage {
   inherit pname version;
@@ -17,12 +16,12 @@ buildNpmPackage {
     sha256 = "sha256-TSG3iCwXcOjxbar7zWtey+JU9XPoSrenDOtU1abCo20=";
   };
 
-  npmDeps = importNpmLock {
-    npmRoot = ./package-lock.json;
-  };
-  inherit (importNpmLock) npmConfigHook;
-
   dontNpmBuild = true;
+
+  npmDepsHash = "sha256-11tfR1NI16By7JzbmsO/NAOKH/H68vG2JjZpwT4ds/Q=";
+  postPatch = ''
+    cp ${./package-lock.json} ./package-lock.json
+  '';
 
   passthru.updateScript = nix-update-script {
     extraArgs = [
