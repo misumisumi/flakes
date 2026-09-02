0997992772be21cb82010f420342780b15b88bb7
misumisumi <s.kobayashi@misumi-sumi.com>
2026-09-02 11:54:43 +0900

    fix: only apply fixed version to anti-anti-cheat-patch and AACPkgs

--- pkgs/apps/default.nix
+++ pkgs/apps/default.nix
@@ -16,16 +16,16 @@ let
     fetchurl
     fetchFromGitHub
     dockerTools
+    edk2
     qemu
     OVMF
-    buildPackages
-    anti-anti-cheat-patch
     ;
 
   inherit (import ../utils.nix { inherit lib python3 callPackage; }) withContents;
 
   ignorePkgs = [
     "aac-edk2-qemu"
+    "anti-anti-cheat-patch"
     "blender-bin"
   ];
 
@@ -33,14 +33,43 @@ let
     filterAttrs (n: v: (all (p: n != p) ignorePkgs) && (v == "directory")) (readDir ./.)
   );
 
-  aacPkgs = import ./aac-edk2-qemu {
-    inherit
-      OVMF
-      anti-anti-cheat-patch
-      buildPackages
-      qemu
-      ;
+  #NOTE: https://codeberg.org/Scrut1ny/AutoVirt not support QEMU=11.0.3, EDK2=202605 or later
+  fixed-qemu = qemu.overrideAttrs (old: rec {
+    version = "11.0.2";
+    src = fetchurl {
+      url = "https://download.qemu.org/qemu-${version}.tar.xz";
+      hash = "sha256-N0X26oji6H/g3IOLKx1OCncL9I4BodWhhoQqH/92zPU=";
+    };
+    enableParallelBuilding = true;
+  });
+  fixed-edk2 = edk2.overrideAttrs (old: rec {
+    version = "202605";
+    srcWithVendoring = fetchFromGitHub {
+      owner = "tianocore";
+      repo = "edk2";
+      tag = "edk2-stable${version}";
+      fetchSubmodules = true;
+      hash = "sha256-sUqLocdX7lxN2pEdn84Cjh8pOzYqIeKqO144XhwKA30=";
+    };
+    __intentionallyOverridingVersion = true;
+  });
+  anti-anti-cheat-patch = callPackage ./anti-anti-cheat-patch {
+    qemu = fixed-qemu;
+    edk2 = fixed-edk2;
   };
+
+  aacPkgs =
+    import ./aac-edk2-qemu {
+      inherit
+        OVMF
+        anti-anti-cheat-patch
+        ;
+      qemu = fixed-qemu;
+      edk2 = fixed-edk2;
+    }
+    // {
+      inherit anti-anti-cheat-patch;
+    };
   blenderPkgs = import ./blender-bin {
     inherit lib;
     inherit (pkgs)
